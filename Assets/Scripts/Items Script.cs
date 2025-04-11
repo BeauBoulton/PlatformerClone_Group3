3b39331dd23c165985edad06ef2fd1d84ebd3284
OnePunchWeeb <nsumek@gmail.com>
2025-04-10 17:27:19 -0700

    sets up item script
    makes an item script and pickup prefabs

---
+++ Assets/Scripts/Items Script.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ *Nick Sumek
+ * 4/ 10 / 25
+ * sets up the item pick ups/ power ups for the game
+ */
+
+
+public class ItemsScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
