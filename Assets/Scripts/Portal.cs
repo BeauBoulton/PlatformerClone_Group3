b4ef5a218d6d67da7fd35d77c706c5443cae9777
OnePunchWeeb <nsumek@gmail.com>
2025-04-15 17:07:16 -0700

    set up the start and end screens with buttons
    Start and end screens are now functional

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -2,6 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ *Nick Sumek
+ * updated 4/ 15 / 25
+ * sets up the portal function
+ */
+
 public class Portal : MonoBehaviour
 {
     public Transform teleportPoint;
